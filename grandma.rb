facfb0d9a0b39ed79d031bd72045185722ac9f35
Shakeria Smith <shakerias2@gmail.com>
2018-08-09 22:50:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if 1+1=2
-    puts I LOVE YOU TOO PUMPKIN!
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
