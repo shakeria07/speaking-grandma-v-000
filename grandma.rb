43847d3c7527ddb9ed44d6f40bbc6ce25381e259
Shakeria Smith <shakerias2@gmail.com>
2018-08-09 22:54:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase?
     puts "I LOVE YOU TOO PUMPKIN!"
 end
-
+end 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
