e4a501c3f5191e5a53d747d93890cd78ff6d8535
Shakeria Smith <shakerias2@gmail.com>
2018-08-09 23:19:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if "string".downcase?
-    puts "HUH?! SPEAK UP, SONNY"
+  if "string".upcase
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
 
 
