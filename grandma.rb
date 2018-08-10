cc4304736a172fa0a273cf10a889f4903009423e
Shakeria Smith <shakerias2@gmail.com>
2018-08-10 00:31:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
-  	return "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  elsif phrase == phrase.upcase
   	return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
