c9a51fc24cfe400ae004975447de883770bcf3d3
Shakeria Smith <shakerias2@gmail.com>
2018-08-10 00:26:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,28 +1,14 @@
 # Write a speak_to_grandma method.
-
-def speak_to_grandma(phrase)
-  if  "phrase".downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif "phrase".upcase
-      return "NO, NOT SINCE 1938!"
-    else  "I LOVE YOU GRANDMA"
-      return "I LOVE YOU TOO PUMPKIN!"
-end
-end
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  
+
   elsif phrase == phrase.downcase
   	return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  
+
   else
   	return "NO, NOT SINCE 1938!"
   end
 end
-
-
-
-
